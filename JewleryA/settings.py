448c54e75b3276ea2be3ba345ba1aea0bf1e409b
Adrian Castro <70278376+kastro0005@users.noreply.github.com>
2024-01-01 23:19:35 -0500

    jjjjjj

--- JewleryA/settings.py
+++ JewleryA/settings.py
@@ -71,12 +71,14 @@ CKEDITOR_UPLOAD_PATH = "/media/"
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'JewleryA.urls'
